96aad2d192f615947f3638864fe434a09ce759e0
Kevin Cai <kevincai96@gmail.com>
2015-11-19 20:59:19 -0500

    Refactored RSA encryption methods

--- src/atrium/Peer.java
+++ src/atrium/Peer.java
@@ -1,52 +1,122 @@
 package atrium;
 
+import java.security.KeyFactory;
 import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.concurrent.CountDownLatch;
 
 import com.esotericsoftware.kryonet.Connection;
 
+import data.Data;
+import data.DataTypes;
+import requests.Request;
+import requests.RequestTypes;
+
 public class Peer {
-	
+
+	private CountDownLatch deferredRequesting;
 	private CountDownLatch cryptoDone;
 	private Connection connection;
 	private PublicKey pubkey;
 	private String mutex;
 	private int inOut;  //= 1 for incoming
-	
+
 	public Peer(Connection connection, int inOut) {
+		deferredRequesting = new CountDownLatch(1);
 		cryptoDone = new CountDownLatch(1);
 		this.connection = connection;
 		this.inOut = inOut;
+
+		//Add ourselves to peers without data
+		NetHandler.peers.add(this);
+		
+		bootstrapRequests();
 	}
 	
+	private void bootstrapRequests() {
+		(new Thread(new Runnable() {
+			public void run() {
+				try {
+					if(inOut == 1) {
+						//Pro-active request approach as a psuedo-server
+						Utilities.log(this, "Sending our pubkey first");
+						connection.sendTCP(new Data(DataTypes.PUBKEY, Core.pubKey));
+						Utilities.log(this, "Requesting peer's pubkey");
+						connection.sendTCP(new Request(RequestTypes.PUBKEY, null));
+						Utilities.log(this, "Awaiting peer's pubkey");
+						cryptoDone.await();
+						Utilities.log(this, "Requesting peer's mutex");
+						connection.sendTCP(new Request(RequestTypes.MUTEX, null));
+						Utilities.log(this, "Requesting peer's peerlist");
+						connection.sendTCP(new Request(RequestTypes.PEERLIST, null));
+					} else {
+						//When we send back a peerList, it's time to start sending requests
+						//Keeping cryptoDone as a sequential check
+						cryptoDone.await();
+						deferredRequesting.await();
+						Utilities.log(this, "Requesting peer's mutex");
+						connection.sendTCP(new Request(RequestTypes.MUTEX, null));
+						Utilities.log(this, "Requesting peer's peerlist");
+						connection.sendTCP(new Request(RequestTypes.PEERLIST, null));
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		})).start();
+	}
+	
+	public void disconnect() {
+		if(mutex != null) {
+			Utilities.log(this, "Peer " + mutex + " disconnected");
+		} else {
+			Utilities.log(this, "Peer disconnected (mutex was null on disconnect");
+		}
+		NetHandler.peers.remove(this);
+		connection.close();
+	}
+
 	public Connection getConnection() {
 		return connection;
 	}
-	
+
 	public PublicKey getPubkey() {
 		return pubkey;
 	}
-	
+
 	public String getMutex() {
 		return mutex;
 	}
-	
+
 	public int getInOut() {
 		return inOut;
+	}
+
+	public CountDownLatch getDeferredLatch() {
+		return deferredRequesting;
 	}
 	
 	public CountDownLatch getCryptoLatch() {
 		return cryptoDone;
 	}
-	
-	public void setPubkey(PublicKey pubkey) {
-		this.pubkey = pubkey;
+
+	public void setPubkey(String pubkey) {
+		try {
+			byte[] pubKeyBytes = pubkey.getBytes();
+			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(pubKeyBytes));
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			PublicKey pk = kf.generatePublic(keySpec);
+			this.pubkey = pk;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
-	
+
 	public void setMutex(String mutex) {
 		this.mutex = mutex;
 	}
-	
+
 	public static Peer findPeer(Connection connection) {
 		for(Peer peer : NetHandler.peers) {
 			if(peer.getConnection().equals(connection)) {
@@ -54,5 +124,24 @@ public class Peer {
 			}
 		}
 		return null;
+	}
+	
+	public void mutexCheck(String mutexData) {
+		if(mutexData.equals(Core.mutex)) {
+			disconnect();
+		} else {
+			boolean passed = true;
+			for(Peer peer : NetHandler.peers) {
+				if(peer != this && peer.getMutex().equals(mutexData)) {
+					passed = false;
+					break;
+				}
+			}
+			if(passed) {
+				setMutex(mutexData);
+			} else {
+				disconnect();
+			}	
+		}
 	}
 }
--- src/crypto/RSA.java
+++ src/crypto/RSA.java
@@ -6,23 +6,23 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import javax.crypto.BadPaddingException;
+import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SealedObject;
 
 import atrium.Core;
 
 public class RSA {
 	
 	private KeyPairGenerator kpg;
-	private KeyPair myPair;
+	public KeyPair myPair;
 	
 	public RSA() throws NoSuchAlgorithmException {
 		kpg = KeyPairGenerator.getInstance("RSA");
 		myPair = kpg.generateKeyPair();
-		Core.pubKey = myPair.getPublic();
+		byte[] pubKeyBytes = myPair.getPublic().getEncoded();
+		Core.pubKey = new String(Base64.getEncoder().encode(pubKeyBytes));
 	}
 	
 	/**
@@ -35,29 +35,26 @@ public class RSA {
 	 * @throws NoSuchAlgorithmException
 	 * @throws NoSuchPaddingException
 	 */
-	public SealedObject encrypt(String str) throws InvalidKeyException, 
-												   IllegalBlockSizeException, 
-												   IOException, 
-												   NoSuchAlgorithmException, 
-												   NoSuchPaddingException {
-		if(myPair == null) {
-			kpg = KeyPairGenerator.getInstance("RSA");
-			myPair = kpg.generateKeyPair();
+	public String encrypt(String str, PublicKey pk) {
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, pk);
+			return new String(cipher.doFinal(str.getBytes()), "ISO-8859-1");
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.ENCRYPT_MODE, myPair.getPublic());
-		return new SealedObject(str, cipher);
+		return null;
 	}
 	
-	public String decrypt(SealedObject in, PublicKey extKey) throws NoSuchAlgorithmException, 
-	                                                                NoSuchPaddingException, 
-	                                                                InvalidKeyException, 
-	                                                                ClassNotFoundException, 
-	                                                                IllegalBlockSizeException, 
-	                                                                BadPaddingException, 
-	                                                                IOException {
-		Cipher dec = Cipher.getInstance("RSA");
-		dec.init(Cipher.DECRYPT_MODE, extKey);
-		return (String) in.getObject(dec);
+	public String decrypt(String in) {
+		try {
+			Cipher decipher = Cipher.getInstance("RSA");
+			decipher.init(Cipher.DECRYPT_MODE, myPair.getPrivate());
+			String output = new String(decipher.doFinal(in.getBytes("ISO-8859-1")));
+			return output;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
 	}
 }
