9a132d9eab085e1627a8537c88affbec99f6667e
Kevin Cai <kevincai96@gmail.com>
2015-12-30 14:16:03 -0500

    Removed debugging for cache poll thread

--- src/net/poll/CachePoller.java
+++ src/net/poll/CachePoller.java
@@ -1,33 +1,31 @@
 package net.poll;
 
+import atrium.Core;
 import atrium.Peer;
 import atrium.Utilities;
 import packets.requests.Request;
 import packets.requests.RequestTypes;
 
 public class CachePoller implements Runnable {
-	
-	private Peer peer;
-	
-	public CachePoller(Peer peer) {
-		this.peer = peer;
-	}
 
 	public void run() {
-		try {
-			Thread.sleep(3000);
-			while(peer.getConnection().isConnected()) {
-				if(peer.getAES() == null) {
-					Thread.sleep(1000);
-					continue;
+		while(true) {
+			try {
+				for(int i=0; i < Core.peers.size(); i++) {
+					Peer peer = Core.peers.get(i);
+					Thread.sleep(3000);
+					if(peer.getConnection().isConnected()) {
+						if(peer.getAES() == null) {
+							continue;
+						}
+						Utilities.log(this, "Polling peer " + peer.getMutex() + " for cache", false);
+						peer.getConnection().sendTCP(new Request(RequestTypes.CACHE, null));
+					}
 				}
-				Utilities.log(this, "Polling peer " + peer.getMutex() + " for cache", false);
-				peer.getConnection().sendTCP(new Request(RequestTypes.CACHE, null));
 				Thread.sleep(90000);
+			} catch(Exception ex) {
+				ex.printStackTrace();
 			}
-			Utilities.log(this, "Terminating cache poller thread for peer " + peer.getMutex(), false);
-		} catch(Exception ex) {
-			ex.printStackTrace();
 		}
 	}
 }
