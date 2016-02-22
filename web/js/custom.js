1ae9fce2c1ef0be4dcfd942fa1988b8063b61337
Kevin Cai <kevincai96@gmail.com>
2016-02-22 16:42:46 -0500

    Pages JS structure / added jQuery debug function

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,44 +1,65 @@
 $(document).ready(function() {
+	// Set initial page as Home
+	setHome();
+
+	// Disable user zoom
+	disableZoom();
+	
+	// Sidebar listeners
+	sidebarOps();
+
 	var formNames = [];
 	processForms(formNames);
 
-	//Port status update
+	// Port status update
 	pollStatus();
 	
-	//Peer number update
+	// Peer number update
 	peerCount();
 	setInterval(peerCount, 1500);
-
-	disableAndSetZoom();
 });
 
-function disableAndSetZoom() {
-
+function disableZoom() {
 	$(window).keydown(function(event) {
 	    if (event.ctrlKey==true && (event.which == '187'  || event.which == '189')) {
 			event.preventDefault();
 		}
 
 	    $(window).bind('mousewheel DOMMouseScroll', function(event) {
-	        if(event.ctrlKey == true) {
+	        if (event.ctrlKey == true) {
 	            event.preventDefault(); 
 	        }
 	    });
 	});
+}
 
-	//window.parent.document.body.style.zoom = 1.20;
+function setHome() {
+	$('#page-text').html($('#home-page').html());
+}
+
+function sidebarOps() {
+	$('.sidebar-nav li a').click(function(event) {
+		var opt = $(this);
+		if (opt.hasClass('nav-option') && !opt.hasClass('active')) {
+			$('.active').removeClass('active');
+			opt.addClass('active');
+			var newText = $('#' + this.id + '-page');
+			$('#page-text').html(newText.html());
+			event.preventDefault();
+		}
+	});
 }
 
 function processForms(formNames) {
 	getAllForms(formNames);
 
 	//Iterate through forms, attaching submit listeners
-	for(i = 0; i < formNames.length; i++) {
+	for (i = 0; i < formNames.length; i++) {
 		var thisForm = formNames[i];
 		var switcher = '#' + thisForm.id;
 
 		//Switcher
-		switch(switcher) {
+		switch (switcher) {
 			case '#searchForm':
 				$(switcher).submit(function(event) {
 					console.log("Handler attch");
@@ -64,7 +85,7 @@ function pollStatus() {
 		method: 'POST',
 		data: '{ "rpc": "port_check" }'
 	}).done(function(result) {
-		if(result.value) {
+		if (result.value) {
 			$('#status').html("<span class=\"label label-success\">OPEN</span>");
 		} else {
 			$('#status').html("<span class=\"label label-danger\">CLOSED</span>");
@@ -83,15 +104,37 @@ function peerCount() {
 		var imageSrc;
 		if(peerCount == 0) {
 			imageSrc = 'img/connect0.png';
-		} else if(peerCount > 0 && peerCount <= 2) {
+		} else if (peerCount > 0 && peerCount <= 2) {
 			imageSrc = 'img/connect1.png';
-		} else if(peerCount > 2 && peerCount <= 4) {
+		} else if (peerCount > 2 && peerCount <= 4) {
 			imageSrc = 'img/connect2.png';
-		} else if(peerCount > 4 && peerCount <= 6) {
+		} else if (peerCount > 4 && peerCount <= 6) {
 			imageSrc = 'img/connect3.png';
-		} else if(peerCount > 6) {
+		} else if (peerCount > 6) {
 			imageSrc = 'img/connect4.png';
 		}
 		$('#peerCount').html("<img src=\"" + imageSrc + "\" width=20 height=20>");
 	});
 }
+
+// Debug jQuery plugin for listing classes
+;!(function ($) {
+    $.fn.classes = function (callback) {
+        var classes = [];
+        $.each(this, function (i, v) {
+            var splitClassName = v.className.split(/\s+/);
+            for (var j in splitClassName) {
+                var className = splitClassName[j];
+                if (-1 === classes.indexOf(className)) {
+                    classes.push(className);
+                }
+            }
+        });
+        if ('function' === typeof callback) {
+            for (var i in classes) {
+                callback(classes[i]);
+            }
+        }
+        return classes;
+    };
+})(jQuery);
