24c6cc5f77dbc8fdff8c24b83d04e8d94c8f7175
Kevin Cai <kevincai96@gmail.com>
2016-04-29 00:35:51 -0500

    Web refactor and streaming removal

--- web/web/js/custom.js
+++ web/web/js/custom.js
@@ -1,5 +1,4 @@
 var lastOnline = -3; // 3 attempts before timeout
-var audioPlayer;
 
 $(document).ready(function() {
 	// Set initial page as Home
@@ -18,82 +17,27 @@ $(document).ready(function() {
 	setInterval(pollStatus, 1500);
 	
 	// Peer number update
-	setInterval(peerCount, 1500);
-
-	audioPlayer = startAudio();
-	audioPlayer.load("file:///C:/Users/kevin/Documents/GitHub/Radiator/web/New Navy - Zimbabwe (Flume Remix).mp3");
+	setInterval(peerCount, 1500, true);
 });
+
+function disableZoom() {
+	$(window).keydown(function(event) {
+	    if (event.ctrlKey==true && (event.which == '187'  || event.which == '189')) {
+			event.preventDefault();
+		}
+
+	    $(window).bind('mousewheel DOMMouseScroll', function(event) {
+	        if (event.ctrlKey == true) {
+	            event.preventDefault(); 
+	        }
+	    });
+	});
+}
 
 function pad(n, width, z) {
 	z = z || '0';
 	n = n + '';
 	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
-
-function startAudio() {
-	$('.progress').on("mousedown", scrubberMouseDownHandler);
-
-	var audio5js = new Audio5js({
-		swf_path: '/statics/swf/audio5js.swf',
-		throw_errors: true,
-		format_time: false,
-		ready: audioPlayerBindings
-	});
-
-	return audio5js;
-}
-
-function audioPlayerBindings() {
-	this.firstPlay = 1;
-
-	$('#play').on("click", playPause.bind(this));
-	$('#play-prev').on("click", moveToStart.bind(this));
-	$('#progress-col').on("mousedown", moveToSeek.bind(this));
-
-	this.on('timeupdate', function (position, duration) {
-		console.log(position);
-		var nicePosition = pad(Math.floor(position / 60), 2);
-		var positionSecLeft = pad(Math.floor(position - (nicePosition * 60)), 2);
-
-		var niceDuration = pad(Math.floor(duration / 60), 2);
-		var secondsLeft = pad(Math.floor(duration - (niceDuration * 60)), 2);
-		if(window.innerWidth < 960) {
-			$('#play-time').html(nicePosition + ":" + positionSecLeft + " " + niceDuration + ":" + secondsLeft);
-		} else {
-			$('#play-time').html(nicePosition + ":" + positionSecLeft + " | " + niceDuration + ":" + secondsLeft);
-		}
-		$('#music-progress').css('width', ((position / duration) * 100) + "%");
-	});
-}
-
-var moveToStart = function () {
-	this.seek(0);
-}
-
-var moveToSeek = function() {
-	var percentSeek = (seekpoint) * this.duration;
-	this.seek(percentSeek);
-	if(!this.playing) {
-		$('#play').click();
-	}
-}
-
-function playPause() {
-	if(this.playing) {
-		this.pause();
-		$('#play').css('background-image', 'url(img/play.svg)');
-	} else {
-		this.play();
-		$('#play').css('background-image', 'url(img/pause.svg)');
-	}
-}
-
-function scrubberMouseDownHandler(e) {
-	var $this = $(this);
-	var x = e.pageX - $this.offset().left;
-	var percent = x / $this.width();
-	$('#music-progress').width((percent * 100) + "%");
-	seekpoint = percent;
 }
 
 function setHome() {
@@ -122,19 +66,27 @@ function hookAllForms() {
 						$('#search-results').html(loadGif);
 						
 						// TODO: actually populating table
-						//
 
-						if(connected()) {
+						if (connected() && peerCount(false) > 0) {
 							$('#search-results').addClass('text-center');
 							$.ajax({
 								url: 'http://localhost:8888/api/search',
+								timeout: 5000,
 								method: 'POST',
-								data: JSON.stringify(dataPack)
-							}).done(function(result) {
-								lastOnline = Date.now();
-								$('#search-results').html(JSON.parse(result).value);
-								$('#search-results').removeClass('text-center');
+								data: JSON.stringify(dataPack),
+								success: function(result) {
+									lastOnline = Date.now();
+									$('#search-results').html(JSON.parse(result).value);
+									$('#search-results').removeClass('text-center');
+								},
+								error: function(XMLHttpRequest, textStatus, errorThrown) {
+									if (XMLHttpRequest.readyState == 0) {
+										console.log("Disconnected af");
+									}
+								}
 							});
+						} else if (peerCount(false) == 0) {
+							$('#search-results').html("No peers are connected :(");
 						} else {
 							$('#search-results').html("No connection was detected :(");
 						}
@@ -147,20 +99,6 @@ function hookAllForms() {
 					alert(this.id);
 			}
 		}
-	});
-}
-
-function disableZoom() {
-	$(window).keydown(function(event) {
-	    if (event.ctrlKey==true && (event.which == '187'  || event.which == '189')) {
-			event.preventDefault();
-		}
-
-	    $(window).bind('mousewheel DOMMouseScroll', function(event) {
-	        if (event.ctrlKey == true) {
-	            event.preventDefault(); 
-	        }
-	    });
 	});
 }
 
@@ -181,14 +119,20 @@ function pollStatus() {
 	if(connected()) {
 		$.ajax({
 			url: 'http://localhost:8888/api/portcheck',
-			timeout: 1000,
-			method: 'GET'
-		}).done(function(result) {
-			lastOnline = Date.now();
-			if (result.value) {
-				$('#status').html("<span class=\"label label-success\">OPEN</span>");
-			} else {
-				$('#status').html("<span class=\"label label-danger\">CLOSED</span>");
+			timeout: 5000,
+			method: 'GET',
+			success: function(result) {
+				lastOnline = Date.now();
+				if (result.value) {
+					$('#status').html("<span class=\"label label-success\">OPEN</span>");
+				} else {
+					$('#status').html("<span class=\"label label-danger\">CLOSED</span>");
+				}
+			},
+			error: function(XMLHttpRequest, textStatus, errorThrown) {
+				if (XMLHttpRequest.readyState == 0) {
+					console.log("Disconnected af");
+				}
 			}
 		});
 	} else {
@@ -196,32 +140,43 @@ function pollStatus() {
 	}
 }
 
-function peerCount() {
-	if(connected()) {
-		$.ajax({
-			url: 'http://localhost:8888/api/peers/count',
-			method: 'GET'
-		}).done(function(result) {
-			lastOnline = Date.now();
-			result = JSON.parse(result);
-			var peerCount = Number(result.value);
-			var imageSrc;
-			if(peerCount == 0) {
-				imageSrc = 'img/connect0.png';
-			} else if (peerCount > 0 && peerCount <= 2) {
-				imageSrc = 'img/connect1.png';
-			} else if (peerCount > 2 && peerCount <= 4) {
-				imageSrc = 'img/connect2.png';
-			} else if (peerCount > 4 && peerCount <= 6) {
-				imageSrc = 'img/connect3.png';
-			} else if (peerCount > 6) {
-				imageSrc = 'img/connect4.png';
-			}
-			$('#peerCount').html("<img src=\"" + imageSrc + "\" width=20 height=20>");
-		});
-	} else {
-		$('#peerCount').html("<img src=\"" + 'img/connect0.png' + "\" width=20 height=20>");
+function peerCount(setLabel) {
+	var peerCount = 0;
+	if (setLabel) {
+		if(connected()) {
+			$.ajax({
+				url: 'http://localhost:8888/api/peers/count',
+				timeout: 5000,
+				method: 'GET',
+				success: function(result) {
+					lastOnline = Date.now();
+					result = JSON.parse(result);
+					peerCount = Number(result.value);
+					var imageSrc;
+					if (peerCount == 0) {
+						imageSrc = 'img/connect0.png';
+					} else if (peerCount > 0 && peerCount <= 2) {
+						imageSrc = 'img/connect1.png';
+					} else if (peerCount > 2 && peerCount <= 4) {
+						imageSrc = 'img/connect2.png';
+					} else if (peerCount > 4 && peerCount <= 6) {
+						imageSrc = 'img/connect3.png';
+					} else if (peerCount > 6) {
+						imageSrc = 'img/connect4.png';
+					}
+					$('#peerCount').html("<img src=\"" + imageSrc + "\" width=20 height=20>");
+				},
+				error: function(XMLHttpRequest, textStatus, errorThrown) {
+					if (XMLHttpRequest.readyState == 0) {
+						console.log("Disconnected af");
+					}
+				}
+			});
+		} else {
+			$('#peerCount').html("<img src=\"" + 'img/connect0.png' + "\" width=20 height=20>");
+		}
 	}
+	return peerCount;
 }
 
 function connected() {
