05a8d219ad67876dee118e9dd448ccd6af8cd055
Kevin Cai <kevincai96@gmail.com>
2016-05-01 16:07:43 -0400

    Switched debug servers

--- src/atrium/NetHandler.java
+++ src/atrium/NetHandler.java
@@ -312,7 +312,7 @@ public class NetHandler {
 
 			// foundHosts.add(InetAddress.getByName("136.167.66.138"));
 			// foundHosts.add(InetAddress.getByName("192.3.165.112"));
-			foundHosts.add(InetAddress.getByName("192.227.251.74"));
+			foundHosts.add(InetAddress.getByName("52.11.190.28"));
 			// foundHosts.add(InetAddress.getByName("136.167.252.240"));
 
 			filterHosts();
